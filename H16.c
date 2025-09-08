e5bb4cf0cda62dc13d755d8dcd424103a471d81d
drift-nova <sahibkhan01841@gmail.com>
2025-09-08 17:51:08 +0600

    Count the Zeros

---
+++ H16.c
@@ -0,0 +1,13 @@
+//in the name of Allah
+#include <stdio.h>
+int main (){
+int a[10],v,c;
+printf("enter :");
+for (v=0;v<10;v++){
+scanf("%d",&a[v]);
+} for (v=0;v<10;v++){
+if(a[v]==0){
+c++;}}
+printf("zero count : %d",c);
+return 0;
+}
