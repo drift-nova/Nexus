46ef1bdf0e463ea038d50461a03c34f387eef083
drift-nova <sahibkhan01841@gmail.com>
2025-09-19 15:58:07 +0600

    Check odd or even using switch inside for loop

---
+++ K2.c
@@ -0,0 +1,17 @@
+//in the name of Allah
+#include <stdio.h>
+int main (){
+
+int joker ;
+for (joker=0;joker<7;joker++){
+
+switch(joker%2){
+
+case 0:printf("\n%d is even",joker);
+break;
+
+case 1:printf("\n%d is odd",joker);
+
+ }}
+return 0;
+}
