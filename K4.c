f59dabf063c8d2bb51dbc68ae0f57786dd11b565
drift-nova <sahibkhan01841@gmail.com>
2025-09-19 16:26:29 +0600

    Print numbers from 1 to N using while loop

---
+++ K4.c
@@ -0,0 +1,15 @@
+//in the name of Allah
+#include <stdio.h>
+int main (){
+
+int dilbar ,i=1;
+printf("enter =");
+scanf ("%d",&dilbar);
+
+while (i<=dilbar){
+
+        printf("\n%d",i);
+i++;}
+
+return 0;
+}
