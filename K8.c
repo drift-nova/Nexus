ddc0a4d77c690f3956c4c8db91ca7d2c01a0cce6
drift-nova <sahibkhan01841@gmail.com>
2025-09-20 00:43:14 +0600

    2D array print

---
+++ K8.c
@@ -0,0 +1,13 @@
+// in the name of Allah
+#include <stdio.h>
+       void main (){
+
+       int the[2][3]={ {3,2,1},{1,2,3} };
+
+       int bat, man ;
+       for(bat=0 ; bat<2 ; bat++){
+       for(man=0 ; man<3 ; man++){
+
+       printf("\n%d",the[bat][man]);
+       }}
+       }
