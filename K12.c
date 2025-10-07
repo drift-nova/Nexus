46c7763cb17f121c44cbb398e435f1ef1a5f3d82
drift-nova <sahibkhan01841@gmail.com>
2025-10-08 00:10:31 +0600

    Even and Odd

---
+++ K12.c
@@ -0,0 +1,24 @@
+//in the name of Allah
+#include <stdio.h>
+void main (){
+
+int arr[10],i;
+printf("enter =");
+for(i=0; i<10; i++){
+scanf("%d",&arr[i]);}
+
+ printf("even =");
+for(i=0;i<10;i++){
+if(arr[i]%2==0){
+printf(" %d",arr[i]);
+}}
+
+printf("\nodd =");
+for(i=0;i<10;i++){
+ if
+(arr[i]%2!=0){
+printf(" %d",arr[i]);
+}
+}
+
+}
